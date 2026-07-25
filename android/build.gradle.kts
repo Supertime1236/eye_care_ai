73e69ca673fcce83d15af21d202ca7898263a83f
Supertime1236 <anhkhoa1572@gmail.com>
2026-07-26 00:19:21 +0700

    Remove defaultConfig and buildTypes sections from Gradle files to streamline configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,32 +22,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
-defaultConfig {
-    applicationId = "com.eyecare.eye_care_ai"
-    minSdk = 26
-    targetSdk = flutter.targetSdkVersion
-    versionCode = 3  // Tăng lên 3 (khớp với +3 ở pubspec)
-    versionName = "1.0.1"  // Thay đổi từ flutter.versionName
-}
-
-// THÊM signingConfigs CHO RELEASE
-signingConfigs {
-    create("release") {
-        storeFile = file("keystore/eye_care_ai.jks")
-        storePassword = System.getenv("KEYSTORE_PASSWORD") ?: "your_password"
-        keyAlias = "eye_care_ai"
-        keyPassword = System.getenv("KEY_PASSWORD") ?: "your_password"
-    }
-}
-
-buildTypes {
-    release {
-        signingConfig = signingConfigs.getByName("release")  // Sửa từ "debug" thành "release"
-        isMinifyEnabled = true
-        proguardFiles(
-            getDefaultProguardFile("proguard-android-optimize.txt"),
-            "proguard-rules.pro"
-        )
-    }
-}
